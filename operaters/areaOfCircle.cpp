0a1feb913aa4938a8130fd46458c8aa1ec44ca2b
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-06-11 16:06:25 +0530

    area

---
+++ operaters/areaOfCircle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r,area;
+    cout<<"enter the value of r:";
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"area of circle is:"<<area;
+    return 0;
+}
