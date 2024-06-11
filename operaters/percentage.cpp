e579832ef38fc05e398a308a0f4e3973939a6fa1
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-06-11 15:59:49 +0530

    percentage of 5 numbers

---
+++ operaters/percentage.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,percentage;
+    cout<<"enter the value of a:";
+    cin>>a;
+    cout<<"enter the value of b:";
+    cin>>b;
+    cout<<"enter the value of c:";
+    cin>>c;
+    cout<<"enter the value of d:";
+    cin>>d;
+    cout<<"enter the value of e:";
+    cin>>e;
+    percentage=(a+b+c+d+e)/5;
+    cout<<"percentage is:"<<percentage;
+    return 0;
+}
