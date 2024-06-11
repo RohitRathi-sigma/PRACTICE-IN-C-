23b0c14957f80f62610206de963638327e09421d
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-06-11 16:15:39 +0530

    simple interest

---
+++ operaters/simpleInterest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int p,r,t,SI;
+    cout<<"enter the value of p:";
+    cin>>p;
+    cout<<"enter the value of r:";
+    cin>>r;
+    cout<<"enter the value of t:";
+    cin>>t;
+    SI=(p*r*t)/100;
+    cout<<"SI is:"<<SI;
+    return 0;
+
+}
