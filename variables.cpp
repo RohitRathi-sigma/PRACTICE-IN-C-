a0880a8cef7a2f26cd640bccba0ac965867e6140
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-06-11 16:01:27 +0530

    multiply of two num

---
+++ variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,multiply;
+cout<<"enter the value of a:";
+cin>>a;
+cout<<"enter the value of b:";
+cin>>b;
+multiply=a*b;
+cout<<"multiply is:"<<multiply;
+return 0;
+}
+
