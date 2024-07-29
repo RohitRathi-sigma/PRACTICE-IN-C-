39fb18d7b6bce71d3b87f21f63eb29d1733de7ec
RohitRathi-sigma <rohitrathi9588@gmail.com>
2024-07-29 18:46:44 +0530

    Taking input from user for string

---
+++ STRING/takingInputOfString.cpp
@@ -0,0 +1,11 @@
+//Taking input from user.
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+   string s;
+   getline(cin,s);  //To get the complite input as output
+   cout<<s;
+   
+}
