71a771728a5a2a51e9b831286ad2f58f0d1dcd7d
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-08-05 19:00:10 +0530

    bubble sort

---
+++ BUBBLE SORT/bubbleSort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int arr[6]={5,4,6,3,2,1};
+    int n=6;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+               // int temp=arr[j];
+               //arr[j]=arr[j+1];
+               // arr[j+1]=temp;
+               swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
