7c039ebbc543ca61eb32fbb197188f2a0fc2d701
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-09-21 22:37:52 +0530

    Print sum using parameters

---
+++ RECURSION/printSumFrom1ToN.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void sum1ToN(int sum , int n){
+    if(n==0){
+        cout<<sum<<endl;
+        return;
+    }
+    sum1ToN(sum+n,n-1);
+}
+int main(){
+    sum1ToN(0,5);
+}
