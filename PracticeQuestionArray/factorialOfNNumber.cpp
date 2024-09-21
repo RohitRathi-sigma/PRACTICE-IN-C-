6fd7dcebb9fe01ecea070969c60199ebc1adf543
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-09-21 21:55:04 +0530

    Factorial of numbers from 1 to n

---
+++ PracticeQuestionArray/factorialOfNNumber.cpp
@@ -0,0 +1,17 @@
+//WAP to calculate the factorial of number from 1 to n.
+#include<iostream>
+using namespace std;
+
+void fact(int n){
+    int f=1;
+    for(int i=1;i<=n;i++){
+        f*=i;
+    cout<<f<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin >>n;
+    fact(n);
+}
