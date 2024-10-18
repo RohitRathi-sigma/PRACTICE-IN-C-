a48d523695c029ef2847cbd9bd0f9bd2f1b63e86
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-10-18 22:25:25 +0530

    Odd numbers

---
+++ operaters/printOddNumUpto100.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+
+cout<<"odd number between 1 to 100"<<endl;
+for(int i=1;i<101;i++)
+{
+    if(i%2!=0)
+    {
+        cout<<i<<"is odd"<<endl;
+    }
+}
+    return 0;
+}
