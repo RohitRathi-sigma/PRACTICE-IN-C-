67bf3606009f6007475a44e956ee622c3218b22e
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-10-25 00:46:30 +0530

    How to print a string

---
+++ STRING/stringPrint.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    char str[5]={'a','b','c','d','e'};
+    for(int i=0;i<5;i++){
+        cout<<str[i]<<endl;
+    }
+    return 0;
+}
