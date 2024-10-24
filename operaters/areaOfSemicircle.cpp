20e68ee62719e2108257744949d9a0ebe2dcdc02
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-10-24 07:54:57 +0530

    Area of semicircle

---
+++ operaters/areaOfSemicircle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r,area;
+    cout<<"enter the value of r:";
+    cin>>r;
+    area=(3.14*r*r)/2;
+    cout<<"area of semi-circle is:"<<area;
+    return 0;
+}
