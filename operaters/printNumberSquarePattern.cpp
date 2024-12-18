9acc41868785b4d07c9a191f816bfef0c446d364
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-12-18 21:11:30 +0530

    Print num in square pattern

---
+++ operaters/printNumberSquarePattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5;
+    int b=5;
+    cout<<"Print number square pattern:"<<endl;
+    for (int i = 1; i <a; i++)
+    {
+        for (int j = 1; j<b; j++)
+        {
+            cout<<j;
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
